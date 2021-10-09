b8d9fcf5c14641d46d131ecfd43ff225fd5a7a8a
James Alan Bush <jamesalanbush1972@gmail.com>
2021-10-09 13:29:09 -0400

    ----

---
+++ ChromaCam/CoverLayout.h
@@ -0,0 +1,13 @@
+//
+//  CoverLayout.h
+//  CollectionPractice
+//
+//  Created by abyssinaong on 2017. 3. 22..
+//  Copyright © 2017년 KimYunseo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverLayout : UICollectionViewFlowLayout
+
+@end
